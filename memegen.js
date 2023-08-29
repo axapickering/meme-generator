37d080ba49ed27b372224d72db5dfbede4de3e2b
Axa Pickering <axapickering@gmail.com>
2023-08-29 12:40:32 -0700

    added delete feature + changed meme creation

--- memegen.js
+++ memegen.js
@@ -1,21 +1,34 @@
-document.addEventListener("DOMContentLoaded", () => {
 const form = document.querySelector("#meme-info");
 const container = document.querySelector("#meme-container");
-const memes = document.querySelectorAll(".meme");
 
 form.addEventListener("submit", event => {
+  // preventing page reload
   event.preventDefault();
-  const topText = document.querySelector("#top-text").value;
-  const bottomText = document.querySelector("#bottom-text").value;
+  // gathering form info to use for meme
+  const topText = document.querySelector("#top-text").value.toUpperCase();
+  const bottomText = document.querySelector("#bottom-text").value.toUpperCase();
   const image = document.querySelector("#meme-image-URL").value;
-  container.insertAdjacentHTML("beforeend",`<div class="meme">
-                                              <img class= "meme-image" src="${image}"></src>
-                                              <h4 class="meme-top-text">${topText}</h4>
-                                              <h4 class="meme-bottom-text">${bottomText}</h4>
-                                            </div>`);
-});alert("EventListener added");
 
-memes.forEach(meme => meme.addEventListener("click",event => {
-                                              event.currentTarget.classList.toggle("selected-meme");
-                                            }));
+  // meme creation
+  let meme = document.createElement("div");
+  meme.classList.add("meme");
+  meme.innerHTML = `<img class= "meme-image" src="${image}"></src>
+  <h4 class="meme-top-text">${topText}</h4>
+  <h4 class="meme-bottom-text">${bottomText}</h4>`;
+  container.appendChild(meme);
+
+  // adding event listener so we can select certain memes
+  meme.addEventListener("click", event => {
+    event.currentTarget.classList.toggle("selected-meme");
+  })
+
+
 });
+
+// allows deletion of selected memes via backspace
+document.addEventListener("keyup", event => {
+  if (event.key === "Backspace") {
+    let selectedMemes = document.querySelectorAll(".selected-meme");
+    selectedMemes.forEach(meme => meme.remove());
+  }
+})
